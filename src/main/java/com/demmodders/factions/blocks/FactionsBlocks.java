dd8b11a11296693e73dd5e37e304c4cf3197e114
strubium <113206902+strubium@users.noreply.github.com>
2024-01-29 11:55:45 -0600

    Add custom clocks

---
+++ src/main/java/com/demmodders/factions/blocks/FactionsBlocks.java
@@ -0,0 +1,13 @@
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class FactonsBlocks{
+
+public static final List<Block> BLOCKS = new ArrayList<Block>();
+  
+public static final Block CORE_BLOCK = new Block("silver_block", Material.IRON);
+public static final Block DISRUPTOR_BLOCK = new Block("silver_block", Material.IRON);
+
+}
